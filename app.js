a6285c488a95a46ae68bad8b5a12498d1e1e4b51
nicollyguerra-cloud <nicolly.guerra@escola.pr.gov.br>
2026-03-27 10:52:11 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+let frases = ["F1", "F2", "F3"];
+let frasesCopiada = [...frases];
+
+
+function geradorMensagem(){
+if(frases.length == 0){
+frases = [...frasesCopiada];
+}
+let indiceGerado = Math.floor(Math.random()*(frases.length));
+let fraseGerada = frases [indiceGerado];
+console.log(fraseGerada);
+document.querySelector("#msg").textContent = fraseGerada;
+frases.splice(indiceGerado,1);
+
+}
